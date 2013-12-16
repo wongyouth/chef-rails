5d3bd084768367cfe3fc41356d2ce20db4751361
Ryan Wang <wongyouth@gmail.com>
2013-12-16 11:42:30 +0800

    config pg_hba

--- roles/postgres-server.rb
+++ roles/postgres-server.rb
@@ -7,7 +7,16 @@ default_attributes({
       # `echo -n 'iloverandompasswordsbutthiswilldo''postgres' | openssl md5 | sed -e 's/^/md5/'`
       "postgres" => "md53175bce1d3201d16594cebf9d7eb3f9d",
       "app" => "md5c256467251d9f08d943740c71ef709bb"
-    }
+    },
+    "config" => {
+      "listen_addresses" => "0.0.0.0"
+    },
+    "pg_hba" => [
+      { type: "local", db: "all", user: "postgres", addr: nil, method: "ident" },
+      { type: "local", db: "all", user: "all", addr: nil, method: "ident" },
+      { type: "host", db: "all", user: "all", addr: "127.0.0.1/32", method: "md5" },
+      { type: "host", db: "all", user: "all", addr: "::1/128", method: "md5" },
+    ]
   }
 })
 
