dd45e30f3ef026e5e76402b7c85cff892f425d61
Ryan Wang <wongyouth@gmail.com>
2015-07-06 18:07:23 +0800

    clone rbenv from http://github.com as source

--- roles/rails-app.rb
+++ roles/rails-app.rb
@@ -2,9 +2,16 @@ name "rails-app"
 description "Rails app Server"
 
 default_attributes({
-  #"rbenv" => {
-  #  "group_users" => ["deploy"]
-  #},
+  "rbenv" => {
+    "git_repository" => "http://github.com/sstephenson/rbenv.git",
+    # "group_users" => ["deploy"]
+  },
+  "ruby_build" => {
+    "git_repository" => "http://github.com/sstephenson/ruby-build.git",
+  },
+  "rbenv_vars" => {
+    "git_repository" => "http://github.com/sstephenson/rbenv_vars.git",
+  },
 })
 
 run_list *%w[
