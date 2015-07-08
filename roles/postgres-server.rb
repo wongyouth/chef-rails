5f17371b8f67f1e79207d5572006926bad87eb83
Ryan Wang <wongyouth@gmail.com>
2015-07-08 17:37:40 +0800

    upgrade postgresql version to 9.4

--- roles/postgres-server.rb
+++ roles/postgres-server.rb
@@ -5,12 +5,13 @@ default_attributes({
   "postgresql" => {
     "password" =>  {
       # `echo -n 'iloverandompasswordsbutthiswilldo''postgres' | openssl md5 | sed -e 's/^/md5/'`
-      "postgres" => "md53175bce1d3201d16594cebf9d7eb3f9d",
-      "app" => "md5c256467251d9f08d943740c71ef709bb"
+      "postgres" => "md53175bce1d3201d16594cebf9d7eb3f9d", # postgres
+      "app" => "md5c256467251d9f08d943740c71ef709bb" # app
     },
     "config" => {
       "listen_addresses" => "0.0.0.0"
     },
+    "version" => "9.4",
     "pg_hba" => [
       { type: "local", db: "all", user: "postgres", addr: nil, method: "ident" },
       { type: "local", db: "all", user: "all", addr: nil, method: "ident" },
